8a0bd9dc8615d700a003c9abd412fa1298ec1f9d
Ignot Zarudko <ascendp6n9@gmail.com>
2020-12-21 21:36:07 +0300

    add cart test

--- IStoreTestingFramework/src/test/java/by/ignot/automation/qa/test/CartTest.java
+++ IStoreTestingFramework/src/test/java/by/ignot/automation/qa/test/CartTest.java
@@ -1,4 +1,24 @@
 package by.ignot.automation.qa.test;
 
-public class CartTest {
+import by.ignot.automation.qa.page.ItemPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CartTest extends CommonSetup{
+
+    @Test
+    public void addToCartTest(String itemUrl){
+        ItemPage itemPage = new ItemPage(driver, itemUrl);
+
+        String itemVendorCode = itemPage
+                .openPage()
+                .getItemVendorCode();
+
+        String addedVendorCode = itemPage
+                .addToCart()
+                .goToCart()
+                .getItemVendorCode();
+
+        Assert.assertEquals(itemVendorCode, addedVendorCode);
+    }
 }
